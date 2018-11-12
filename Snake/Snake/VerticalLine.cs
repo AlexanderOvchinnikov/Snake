242442d7e9177f6cacf615d46796e1c56e21c44c
AlexanderOlegovich <ovalex@bk.ru>
2018-11-12 16:22:44 +0300

    Добавлен класс Вертикальная линия

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yTop, int yBot, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int i=yTop; i<=yBot;i++)
+            {
+                Point p = new Point(x, i, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
