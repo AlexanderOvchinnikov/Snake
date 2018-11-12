dc655d065eb3a4b515583f08b0b88a62c8f64407
AlexanderOlegovich <ovalex@bk.ru>
2018-11-12 16:22:05 +0300

    Добавлены классы горизонтальная и вертикальная линии. Внесены изменения в программ.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,17 +14,12 @@ namespace Snake
             Point p2 = new Point(4,5,'#');
             Point p3 = new Point(7,14,'%');
             Point p4 = new Point(21, 4, '&');
-           
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
 
-            p1.Draw();
-            p2.Draw();
-            p3.Draw();
-            p4.Draw();
+            HorizontalLine Hline = new HorizontalLine(5, 15, 8, '+');
+            Hline.Draw();
+            VerticalLine Vline = new VerticalLine(4, 12, 10, '&');
+            Vline.Draw();
+
 
             Console.ReadLine();
 
