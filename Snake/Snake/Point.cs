b5e3060437b0490857d298ba9e5ddafbcda2f81a
AlexanderOlegovich <ovalex@bk.ru>
2018-11-13 13:47:27 +0300

    Добавили конструктор который принимает точку и метод Move

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -23,6 +23,33 @@ namespace Snake
             sym = sym1;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset,Direction direction)
+        {
+            if (direction == Direction.Right)
+            {
+                x = x + offset;
+            }
+            else if (direction== Direction.Left)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.Up)
+            {
+                y = y - offset;
+            }
+            else if (direction == Direction.Down)
+            {
+                y = y + offset;
+            }
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
